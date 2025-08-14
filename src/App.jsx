7fdff409723766d934d3cb7e2db292e9de2e4c4d
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-08-14 21:12:01 +0100

    import header path corrected and base removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Header from './components/Header.jsx'
+import Header from './components/header.jsx'
 import Main from './components/main.jsx'
 import Footer from './components/footer.jsx'
 
